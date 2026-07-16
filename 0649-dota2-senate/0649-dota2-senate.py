d81776f6211e8271861e6c123a407b97575d853c
devanshuawasthi-gif <apdevansh2810@gmail.com>
2026-07-16 23:03:59 +0530

    Time: 11 ms (79.82%), Space: 19.6 MB (34.99%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+
+        radiant = deque()
+        dire = deque()
+
+        # Store indices of senators
+        for i, ch in enumerate(senate):
+            if ch == 'R':
+                radiant.append(i)
+            else:
+                dire.append(i)
+
+        # Simulate the voting rounds
+        while radiant and dire:
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            if r < d:
+                radiant.append(r + n)
+            else:
+                dire.append(d + n)
+
+        return "Radiant" if radiant else "Dire"
+        
