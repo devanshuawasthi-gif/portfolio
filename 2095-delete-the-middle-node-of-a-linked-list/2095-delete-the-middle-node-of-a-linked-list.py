62cf005c3348ea02f699574788d54ddf78d0f649
devanshuawasthi-gif <apdevansh2810@gmail.com>
2026-07-16 23:13:52 +0530

    Time: 56 ms (97.66%), Space: 91.7 MB (32.48%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def deleteMiddle(self, head):
+        if head is None or head.next is None:
+            return None
+        slow = head
+        fast = head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = slow.next
+        return head      
