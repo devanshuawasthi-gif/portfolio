8c5b04aab07d066d62fe107f3cab110e8d7eba1e
devanshuawasthi-gif <apdevansh2810@gmail.com>
2026-08-09 22:11:55 +0530

    Time: 67 ms (94.78%), Space: 26 MB (57.27%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def stoneGameII(self, piles):
+        n = len(piles)
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def dp(i, M):
+            if i + 2 * M >= n:
+                return suffix[i]
+
+            best = 0
+
+            for X in range(1, 2 * M + 1):
+                opponent = dp(i + X, max(M, X))
+                best = max(best, suffix[i] - opponent)
+
+            return best
+
+        return dp(0, 1)
