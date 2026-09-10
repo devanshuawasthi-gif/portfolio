4e854f65e57712dfd439ebf4a6875a615b0c5f1b
devanshuawasthi-gif <apdevansh2810@gmail.com>
2026-09-10 21:19:56 +0530

    Time: 10 ms (39.19%), Space: 15.7 MB (64.72%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,39 @@
+class Pair {
+public:
+    int first;
+    int second;
+
+    Pair(int first, int second) {
+        this->first = first;
+        this->second = second;
+    }
+};
+
+class Solution {
+    int count = 0;
+
+    Pair postOrder(TreeNode* root) {
+        if(root == nullptr) {
+            return Pair(0, 0);
+        }
+
+        Pair left = postOrder(root->left);
+        Pair right = postOrder(root->right);
+
+        int sum = left.first + right.first + root->val;
+        int nodeCount = left.second + right.second + 1;
+
+        if(sum / nodeCount == root->val) {
+            count++;
+        }
+
+        return Pair(sum, nodeCount);
+    }
+
+public:
+    int averageOfSubtree(TreeNode* root) {
+        count = 0;
+        postOrder(root);
+        return count;
+    }
+};
